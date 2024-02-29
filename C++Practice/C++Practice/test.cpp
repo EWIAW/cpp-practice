b622c005257ef57322d6b5a752daedb265100130
EWIAW <2450925561@qq.com>
2024-02-29 23:53:08 +0800

    C++练习

---
+++ C++Practice/C++Practice/test.cpp
@@ -0,0 +1,57 @@
+//#include<iostream>
+
+//using namespace std;
+//
+//int main()
+//{
+//	cout << "hehe\n" << endl;
+//
+//	int a = 10;
+//	double b = 3.1415926;
+//	char ch = 'c';
+//
+//	char arr[] = "haha";
+//
+//	cout << a << " " << b << " " << ch << " " << arr << endl;
+//
+//	return 0;
+//}
+
+//using std::cout;
+//using std::endl;
+//
+//int main()
+//{
+//	cout << "hehe" << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	std::cout << "hehe" << std::endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//double Add(double x, double y)
+//{
+//	return x + y;
+//}
+//
+//
+//
+//int main()
+//{
+//	Add(1, 2);
+//	Add(3.1, 2.5);
+//
+//	return 0;
+//}
+
