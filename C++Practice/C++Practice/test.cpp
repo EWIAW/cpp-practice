c23714e36b1e545c76b88d8405cb23d1071424be
EWIAW <2450925561@qq.com>
2024-03-09 23:46:15 +0800

    C++练习

--- C++Practice/C++Practice/test.cpp
+++ C++Practice/C++Practice/test.cpp
@@ -837,3 +837,214 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	Date(int year = 0, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Show()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//
+//	Date(const Date& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1(2024, 3, 9);
+//	d1.Show();
+//
+//	Date d2(d1);
+//	d2.Show();
+//
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//
+//class Time
+//{
+//public:
+//	Time(int hour = 24, int min = 0, int second = 0)
+//	{
+//		_hour = hour;
+//		_min = min;
+//		_second = second;
+//	}
+//
+//private:
+//	int _hour;
+//	int _min;
+//	int _second;
+//};
+//
+//class Date
+//{
+//public:
+//	Date(int year = 0, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	bool operator==(const Date& d)
+//	{
+//		return _year == d._year
+//			&& _month == d._month
+//			&& _day == d._day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//
+//int main()
+//{
+//	Date d1(2024, 3, 9);
+//	Date d2(2024, 3, 9);
+//
+//	//bool ret = d1.operator==(d2);
+//	bool ret = d1 == d2;
+//
+//	cout << ret << endl;
+//
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+
+class Date
+{
+public:
+	//获取当月的天数
+	int GetMonthDay(int year, int month)
+	{
+		int arr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+		//判断是否闰年
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		{
+			arr[2]++;//二月份天数++
+		}
+		return arr[month];
+	}
+
+	//构造函数
+	Date(int year = 0, int month = 0, int day = 0)
+	{
+		if (year >= 0 
+			&& month >= 1 && month <= 12 
+			&& day >= 1 && day <= GetMonthDay(year, month))
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+		{
+			/*cout << "非法日期" << endl;*/
+			_year = -1;
+			_month = -1;
+			_day = -1;
+		}
+	}
+
+	//拷贝构造
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+
+	//日期比较
+	bool operator==(const Date& d)//判断日期是否相等
+	{
+		if (_year == d._year
+			&& _month == d._month
+			&& _day == d._day)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool operator>(const Date& d)//判读日期是否大于日期
+	{
+		if (_year > d._year)
+		{
+			return true;
+		}
+		else if (_year == d._year && _month > d._month)
+		{
+			return true;
+		}
+		else if (_year == d._year && _month == d._month && _day > d._day)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool operator>=(const Date& d)//判读日期是否大于等于日期
+	{
+		return (*this == d) || (*this > d);
+	}
+
+	bool operator<(const Date& d)//判断日期是否小于日期
+	{
+		return !(*this >= d);
+	}
+
+	//日期-日期
+
+	//日期加天数
+
+	//输出信息函数
+	void Show()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1(2024, 3, 9);
+	Date d2(2024, 3, 8);
+
+	cout << (d1 < d2) << endl;
+
+	return 0;
+}
