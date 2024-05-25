adaca11dd7e07a46e1ec997f0db1d20b8ce7cbc4
EWIAW <2450925561@qq.com>
2024-05-25 22:32:03 +0800

    完善红黑树的插入

--- RBTree/RBTree/RBTree.h
+++ RBTree/RBTree/RBTree.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<iostream>
+#include<vector>
+#include<time.h>
 using namespace std;
 
 enum Color
@@ -32,7 +34,7 @@ class RBTree
 public:
 	typedef RBTreeNode<K,V> Node;
 public:
-	bool insert(const pair<K,V>& tmp)
+	bool insert(const pair<K, V>& tmp)
 	{
 		//如果根节点为空，则直接将新结点给到_root
 		if (_root == nullptr)
@@ -79,19 +81,222 @@ public:
 
 		//插入新结点后，要进行检查，是否要调整
 
-		//1.如果cur的父结点为BLACK，则不需要调整
-		//2.如果cur的父结点为RED，且uncle结点存在且为红，则需要调整
-		//3.如果cur的父结点为RED，且uncle结点不存在，则需要调整
-		//4.如果cur的父结点为RED，且uncle结点存在且为BLACK，则需要调整
+		//一.如果cur的父结点为BLACK，则不需要调整
 
-		Node* grandParent = curParent->_parent;
+		//二.如果cur的父结点为RED，且uncle结点存在且为红，则需要调整
 
-		if (curParent->_col == BLACK)
+		//三.①如果cur的父结点为RED，且uncle结点不存在，则需要调整(单旋) 
+		//  .②如果cur的父结点为RED，且uncle结点存在且为BLACK，则需要调整（单旋）
+
+		//四.①如果cur的父结点为RED，且uncle结点不存在，则需要调整(双旋) 
+		//  .②如果cur的父结点为RED，且uncle结点存在且为BLACK，则需要调整（双旋）
+
+		
+		//我们可以把情况二、三、四 分为 两种情况，分别是 curparent在grangparent的_left 、 curparent在grangparent的_right
+		while (curParent != nullptr && curParent->_col == RED)
 		{
+			Node* grandparent = curParent->_parent;
+			if (grandparent->_left == curParent)
+			{
+				Node* uncle = grandparent->_right;
+				if (uncle != nullptr && uncle->_col == RED)//情况二
+				{
+					curParent->_col = uncle->_col = BLACK;
+					grandparent->_col = RED;
+
+					//继续向上处理
+					cur = grandparent;
+					curParent = cur->_parent;
+				}
+				else
+				{
+					//情况四
+					if (curParent->_right == cur)//说明此时是一个折线，也就是情况四，先处理情况四的原因是，情况四经过一个单旋后会变成情况三
+					{
+						RotateL(curParent);
+						swap(cur, curParent);
+					}
+					//情况三
+					RotateR(grandparent);
+					curParent->_col = BLACK;
+					grandparent->_col = RED;
+					break;
+				}
+			}
+			else
+			{
+				Node* uncle = grandparent->_left;
+				if (uncle != nullptr && uncle->_col == RED)
+				{
+					curParent->_col = uncle->_col = BLACK;
+					grandparent->_col = RED;
+
+					cur = grandparent;
+					curParent = cur->_parent;
+				}
+				else
+				{
+					if (curParent->_left == cur)
+					{
+						RotateR(curParent);
+						swap(cur, curParent);
+					}
+					RotateL(grandparent);
+					curParent->_col = BLACK;
+					grandparent->_col = RED;
+					break;
+				}
+			}
+		}
+		_root->_col = BLACK;
+
+	}
+
+	void RotateR(Node* first)
+	{
+		Node* second = first->_left;
+		Node* secondright = second->_right;
+
+		first->_left = secondright;
+		if (secondright != nullptr)
+		{
+			secondright->_parent = first;
+		}
+
+		second->_right = first;
+		Node* ppNode = first->_parent;
+		first->_parent = second;
+
+		if (first == _root)
+		{
+			_root = second;
+			second->_parent = nullptr;
+		}
+		else
+		{
+			if (ppNode->_left == first)
+			{
+				ppNode->_left = second;
+				second->_parent = ppNode;
+			}
+			else
+			{
+				ppNode->_right = second;
+				second->_parent = ppNode;
+			}
+		}
+
+	}
+
+	void RotateL(Node* first)
+	{
+		Node* second = first->_right;
+		Node* secondleft = second->_left;
+
+		first->_right = secondleft;
+		if (secondleft != nullptr)
+		{
+			secondleft->_parent = first;
+		}
+
+		second->_left = first;
+		Node* ppNode = first->_parent;
+		first->_parent = second;
+
+		if (first == _root)
+		{
+			_root = second;
+			second->_parent = nullptr;
+		}
+		else
+		{
+			if (ppNode->_left == first)
+			{
+				ppNode->_left = second;
+				second->_parent = ppNode;
+			}
+			else
+			{
+				ppNode->_right = second;
+				second->_parent = ppNode;
+			}
+		}
+	}
+
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr)
+		{
+			return;
+		}
+
+		_InOrder(root->_left);
+		cout << root->_kv.first << ":" << root->_kv.second << endl;
+		_InOrder(root->_right);
+	}
+
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+
+	Node* GetRoot()
+	{
+		return _root;
+	}
+
+	bool IsValidRBTree()
+	{
+		Node* pRoot = GetRoot();
+		// 空树也是红黑树
+		if (nullptr == pRoot)
+			return true;
+		// 检测根节点是否满足情况
+		if (BLACK != pRoot->_col)
+		{
+			cout << "违反红黑树性质二：根节点必须为黑色" << endl;
+			return false;
+		}
+		// 获取任意一条路径中黑色节点的个数
+		size_t blackCount = 0;
+		Node* pCur = pRoot;
+		while (pCur)
+		{
+			if (BLACK == pCur->_col)
+				blackCount++;
+			pCur = pCur->_left;
+		}
+		// 检测是否满足红黑树的性质，k用来记录路径中黑色节点的个数
+		size_t k = 0;
+		return _IsValidRBTree(pRoot, k, blackCount);
+	}
+
+	bool _IsValidRBTree(Node* pRoot, size_t k, const size_t blackCount)
+	{
+		//走到null之后，判断k和black是否相等
+		if (nullptr == pRoot)
+		{
+			if (k != blackCount)
+			{
+				cout << "违反性质四：每条路径中黑色节点的个数必须相同" << endl;
+				return false;
+			}
 			return true;
 		}
-		
+		// 统计黑色节点的个数
+		if (BLACK == pRoot->_col)
+			k++;
+		// 检测当前节点与其双亲是否都为红色
+		Node* pParent = pRoot->_parent;
+		if (pParent && RED == pParent->_col && RED == pRoot->_col)
+		{
+			cout << "违反性质三：没有连在一起的红色节点" << endl;
+			return false;
+		}
+		return _IsValidRBTree(pRoot->_left, k, blackCount) &&
+			_IsValidRBTree(pRoot->_right, k, blackCount);
 	}
-private:
-	Node* _root = nullptr;
+
+	private:
+		Node* _root = nullptr;
 };
--- RBTree/RBTree/test.cpp
+++ RBTree/RBTree/test.cpp
@@ -1,7 +1,42 @@
 #include"RBTree.h"
 
+void Test1()
+{
+	int arr[] = { 16,3,7,11,9,26,18,14,15 };
+	//int arr[] = { 4,2,6,1,3,5,15,7,16,14 };
+	RBTree<int, int> root;
+	for (auto& e : arr)
+	{
+		root.insert(make_pair(e, e));
+	}
+	root.InOrder();
+
+	cout << root.IsValidRBTree() << endl;
+}
+
+void Test2()
+{
+	srand(time(0));
+	const int n = 1000000;
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		v.push_back(rand());
+	}
+
+	int begin = clock();
+	RBTree<int, int> root;
+	for (int i = 0; i < n; i++)
+	{
+		root.insert(make_pair(v[i], v[i]));
+	}
+	int end = clock();
+	cout << end - begin << endl;
+}
+
 int main()
 {
-
+	//Test1();
+	Test2();
 	return 0;
 }
